767da7bebe72308eed824de48a98d30bbac6f183
Nathan Bean <screamster22@gmail.com>
2022-06-16 22:26:40 +0000

    Added s3 bucket module 5th time's a charm, hopefully.

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,6 @@
 module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+  source = "app.terraform.io/nathanbean-training/s3-bucket/aws"
 
-  bucket = "my-s3-bucket"
   acl    = "private"
   bucket_prefix = "${var.prefix}"
   version = "2.8.0"
